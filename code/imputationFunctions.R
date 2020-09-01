80046fc159610fa4fa36fed6d19f70f409c9cd2a
wolfemd <wolfemd@gmail.com>
2020-09-01 12:33:31 -0400

    Final version of functions as of completed analysis. Includes new mergeVCFs() and filter_positions() functions.

--- code/imputationFunctions.R
+++ code/imputationFunctions.R
@@ -1,23 +1,24 @@
 splitVCFbyChr<-function(Chr,vcfIn,filters=NULL,outPath,outSuffix){
-  system(paste0("vcftools --gzvcf ",vcfIn,
+  system(paste0("vcftools --gzvcf ",vcfIn," ",
                 "--chr ",Chr," ",filters," ",
                 "--recode --stdout | bgzip -c -@ 24 > ",
                 outPath,"chr",Chr,"_",outSuffix,".vcf.gz")) }
 
 runBeagle5<-function(targetVCF,refVCF,mapFile,outName,
-                     nthreads,maxmem="500g",impute=TRUE,ne=100000){
+                     nthreads,maxmem="500g",impute=TRUE,ne=100000,samplesToExclude=NULL){
   system(paste0("java -Xms2g -Xmx",maxmem," -jar /programs/beagle/beagle.jar ",
                 "gt=",targetVCF," ",
                 "map=",mapFile," ",
                 "ref=",refVCF," ",
                 "out=",outName," ",
-                "nthreads=",nthreads," impute=",impute," ne=",ne)) }
+                "nthreads=",nthreads," impute=",impute," ne=",ne,
+                ifelse(!is.null(samplesToExclude),paste0(" excludesamples=",samplesToExclude),""))) }
 
 postImputeFilter<-function(inPath=NULL,inName,outPath=NULL,outName){
-  require(magrittr)
+  require(magrittr); require(dplyr)
   # Extract imputation quality scores (DR2 and AF) from VCF
-  system(paste0("vcftools --gzvcf ",inPath,inName," --get-INFO DR2 --get-INFO AF --out ",outPath,inName,".vcf.gz"))
-  system(paste0("vcftools --gzvcf ",inPath,inName," --hardy --out ",outPath,inName,".vcf.gz"))
+  system(paste0("vcftools --gzvcf ",inPath,inName,".vcf.gz --get-INFO DR2 --get-INFO AF --out ",outPath,inName))
+  system(paste0("vcftools --gzvcf ",inPath,inName,".vcf.gz --hardy --out ",outPath,inName))
 
   # Read scores into R
   INFO<-read.table(paste0(outPath,inName,".INFO"),
@@ -45,9 +46,37 @@ postImputeFilter<-function(inPath=NULL,inName,outPath=NULL,outName){
               file = paste0(outPath,inName,".sitesPassing"),
               row.names = F, col.names = F, quote = F)
   # Apply filter to vcf file with vcftools
-  system(paste0("vcftools --gzvcf ",inPath,inName," ",
+  system(paste0("vcftools --gzvcf ",inPath,inName,".vcf.gz"," ",
                 "--positions ",outPath,inName,".sitesPassing"," ",
                 "--recode --stdout | bgzip -c -@ 24 > ",
                 outPath,outName,".vcf.gz"))
   print(paste0("Filtering Complete: ",outName))
 }
+
+mergeVCFs<-function(inPath=NULL,inVCF1,inVCF2,outPath=NULL,outName){
+  system(paste0("tabix -f -p vcf ",inPath,inVCF1,".vcf.gz"))
+  system(paste0("tabix -f -p vcf ",inPath,inVCF2,".vcf.gz"))
+  system(paste0("bcftools merge ",
+                "--output ",outPath,outName,".vcf.gz ",
+                "--merge snps --output-type z --threads 6 ",
+                inPath,inVCF1,".vcf.gz"," ",
+                inPath,inVCF2,".vcf.gz"))
+}
+
+filter_positions<-function(inPath=NULL,inVCF,positionFile,outPath=NULL,outName){
+  system(paste0("vcftools --gzvcf ",inPath,inVCF," ",
+                "--positions ",inPath,positionFile," ",
+                "--recode --stdout | bgzip -c -@ 24 > ",
+                outPath,outName,".vcf.gz"))
+}
+
+
+
+# targetVCF=paste0(targetVCFpath,"chr",1,"_DCas20_5360.vcf.gz")
+# refVCF=paste0(refVCFpath,"chr",1,"_ImputationReferencePanel_EMBRAPA_Phased_102619.vcf.gz")
+# mapFile=paste0(mapPath,"chr",1,"_cassava_cM_pred.v6_91019.map")
+# outName=paste0(outPath,"chr",1,"_DCas20_5360_REFimputed")
+# nthreads=112
+# maxmem="500g"
+# impute=TRUE
+# ne=100000
